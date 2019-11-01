386605decc3865e9ba5c0b7132aea69b31773020
Mark Olsson <post@markolsson.se>
2019-11-01 19:27:08 +0100

    Adds some logging

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
+console.log("Setting up serial port")
 const port = new SerialPort('/dev/tty.wchusbserialfd130', { baudRate: 38400 })
-
 port.on('error', function (err) {
     console.log(err.message)
 })
 
+console.log("Setting up listener")
 const parser = new Readline()
 port.pipe(parser)
-
 parser.on('data', line => console.log(`> ${line}`))
+
+console.log("Sending power on command")
 port.write(new Buffer.from([0x80, 0x70, 0xC0, 0x3F]))
