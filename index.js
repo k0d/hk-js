0544dca1968714eebd25d1ea120253253b3adb64
Mark Olsson <post@markolsson.se>
2019-11-01 19:10:17 +0100

    Added serial test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Test OK!")
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('/dev/tty.wchusbserialfd130', { baudRate: 38400 })
+
+const parser = new Readline()
+port.pipe(parser)
+
+parser.on('data', line => console.log(`> ${line}`))
+port.write(new Buffer([0x80, 0x70, 0xC0, 0x3F]))
