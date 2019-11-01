10f3e83b8a99f2db18ac071f4e19ed15fe070227
Mark Olsson <post@markolsson.se>
2019-11-01 19:22:09 +0100

    Some fixes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const port = new SerialPort('/dev/tty.wchusbserialfd130', { baudRate: 38400 })
 
+port.on('error', function (err) {
+    console.log(err.message)
+})
+
 const parser = new Readline()
 port.pipe(parser)
 
 parser.on('data', line => console.log(`> ${line}`))
-port.write(new Buffer([0x80, 0x70, 0xC0, 0x3F]))
+port.write(new Buffer.from([0x80, 0x70, 0xC0, 0x3F]))
