cfea611319fb8856ac19dad99bbc5838a7965d8a
Mark Olsson <post@markolsson.se>
2019-11-01 20:28:54 +0100

    Fixed the packet CRC

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ port.on('error', function (err) {
 console.log("Setting up listener")
 const parser = new Readline()
 port.pipe(parser)
-parser.on('data', line => console.log(`> ${line}`))
+parser.on('data', line => console.log(`-> ${line}`))
 
 console.log("Sending power on command")
-const cmd = new Buffer.from([0x02, 0x04, 0x80, 0x70, 0xC0, 0x3F, (0x70 ^ 0x3F), (0x80 ^ 0xC0)])
-const dgram = 'PCSEND' + cmd
-port.write(dgram)
+const cmd = new Buffer.from([0x50, 0x43, 0x53, 0x45, 0x4e, 0x44, 0x02, 0x04, 0x80, 0x70, 0xc0, 0x3f, 0x40, 0x4f])
+console.log("<- ", cmd)
+port.write(cmd)
